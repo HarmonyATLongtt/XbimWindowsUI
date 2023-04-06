9905e1ab85c2f6c30d7f9cc5d48521abf5db2320
longtt <38779673+longt61@users.noreply.github.com>
2023-04-06 17:08:50 +0700

    update make color

---
+++ Xbim.Presentation/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xbim.Presentation.Enums
+{
+	public enum LODs
+	{
+		Default = 0,
+		LOD_100 = 1,
+		LOD_200 = 2,
+		LOD_300 = 3,
+		LOD_400 = 4,
+		LOD_500 = 5,
+	}
+}
